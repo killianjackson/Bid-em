31ee654bb764ec57a2a569836b7debb547e93deb
Killian Jackson <killianjackson@Killians-MacBook-Pro.local>
2016-02-24 13:16:07 -0800

    First commit of Bid-em UI design.  Beginning layout of Rushee list page

---
+++ Bid-em/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  social-network
+//
+//  Created by Killian Jackson on 1/31/16.
+//  Copyright © 2016 Killian Jackson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0
+
+// Keys
+let KEY_UID = "uid"
+
+// Segue Identifiers
+let SEGUE_LOGGED_IN = "loggedIn"
+
+// Status Codes
+let STATUS_ACCOUNT_NONEXIST = -8
